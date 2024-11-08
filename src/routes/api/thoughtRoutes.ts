71e70608957f13a5c9ed2d812709627e334df622
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-11-07 19:49:19 -0800

    added the routes for thoughtRoutes.ts

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} from '../../controllers/thoughtController';
+
+const router = Router();
+
+// Routes for thoughts
+//api/thoughts/
+router.route('/').get(getAllThoughts).post(createThought);
+//api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// Routes for reactions on a specific thought
+// api/thoughts/:thoughtId/reactions/
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+export default router;
