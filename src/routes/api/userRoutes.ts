6e77a9350fb4bb6a4aa6d945e524ca4d4d2345fb
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-11-07 19:30:11 -0800

    added the uuserRoutes.

---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} from '../../controllers/userController';
+
+const router = Router();
+
+// Route to handle users
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// Routes for a specific user
+// /api/users/:id
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// Routes for adding/removing friends
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+export default router;
